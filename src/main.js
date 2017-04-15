4b93450d7ba1712a3c88e30dc42dcfa27d310b42
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-04-15 10:49:48 +0200

    Bootstrap, Redux, Revue: primo esempio con contatore

--- src/main.js
+++ src/main.js
@@ -1,14 +1,50 @@
 import Vue from 'vue'
+import Revue from 'revue'
+import {createStore, combineReducers} from 'redux'
+
+// create a counter reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+// action creators
+const actions = {
+  increment () {
+    return {type: 'increment'}
+  },
+  decrement () {
+    return {type: 'decrement'}
+  }
+}
+
+// combine reducers
+// and create the redux store
+const reduxStore = createStore(combineReducers({counter}))
+
+// create a revue store by binding redux store to Vue
+const store = new Revue(Vue, reduxStore, actions)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  data: {
-    soggetto: {
-      'nominativo': 'Mario Rossi',
-      'indirizzo': 'via roma 1, Jesi(AN)'
+  data () {
+    return {count: this.$select('counter as count')}
+  },
+  methods: {
+    handleIncrement () {
+      store.dispatch(actions.increment())
+    },
+    handleDecrement () {
+      store.dispatch(actions.decrement())
     }
   }
 })
